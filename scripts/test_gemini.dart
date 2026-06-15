d3c204b28e437c9fd93a4874c29d4c6c086d298c
anwr-1 <nnasr9999@gmail.com>
2026-06-15 08:55:56 +0300

    Update test_gemini.dart

--- scripts/test_gemini.dart
+++ scripts/test_gemini.dart
@@ -4,8 +4,8 @@ import 'package:dio/dio.dart';
 
 void main() async {
   final dio = Dio();
-  final geminiApiKey = 'AQ.Ab8RN6JPrBya6gVrIRK9HOD-I4yLttzsvT3dgGW5p2y4MywF8Q';
-  final geminiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent';
+  final geminiApiKey = '';
+  final geminiUrl = '';
 
   // Create a dummy image (1x1 transparent png)
   final base64Image = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
