6b7d5efda0e17d2a46bbd2864a86ec6beb964be0
anwr-1 <nnasr9999@gmail.com>
2026-06-15 08:55:15 +0300

    Update test_gemini.py

--- scripts/test_gemini.py
+++ scripts/test_gemini.py
@@ -3,8 +3,7 @@ import json
 
 url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent"
 headers = {
-    "Content-Type": "application/json",
-    "X-goog-api-key": "AQ.Ab8RN6JPrBya6gVrIRK9HOD-I4yLttzsvT3dgGW5p2y4MywF8Q"
+
 }
 data = {
     "contents": [
